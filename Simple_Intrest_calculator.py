d9a3f723e766db86f70de6feac1b45f4b6645c85
sunilbudagavi <123731482+sunilbudagavi@users.noreply.github.com>
2024-06-11 12:40:26 +0530

    Add files via upload

---
+++ Simple_Intrest_calculator.py
@@ -0,0 +1,7 @@
+ppl = int(input("Enter the Principle Amount: "))
+Rate = float(input("Enter the Rate: "))
+Time = int(input("Enter the Years: "))
+
+Sample_intrest = ppl * Rate * Time
+print(Sample_intrest)
+
